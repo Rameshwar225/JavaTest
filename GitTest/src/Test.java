6d66d013cd88d12d6a11339aa20755e48987161c
Rameshwar Gosavi <rameshwargosavi1212@gmail.com>
2023-09-24 12:09:21 +0530

    test
    test

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -4,11 +4,12 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Inside main method");
 		System.out.println("Inside main method second");
-<<<<<<< Updated upstream
+
 		System.out.println("Inside main method fourth");
-=======
+
 		System.out.println("Inside main method third");
->>>>>>> Stashed changes
+		System.out.println("Inside main method end");
+
 	}
 
 }
