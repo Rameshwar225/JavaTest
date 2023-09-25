e6005bac941cd84e2537d38ce2b33e4ebc5503d8
Rameshwar Gosavi <rameshwargosavi1212@gmail.com>
2023-09-25 10:32:42 +0530

    Update Test.java

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -4,12 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Inside main method");
 		System.out.println("Inside main method second");
-
-		
-
 		System.out.println("Inside main method third");
 		System.out.println("Inside main method end");
-
 	}
 
 }
